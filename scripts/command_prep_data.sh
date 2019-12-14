0c5b8040707e4497ee2fe7bc3445462cf31ac9e0
huy-ha <hqh2101@columbia.edu>
2019-12-14 16:49:20 -0500

    just use standard velodyne folder

--- scripts/command_prep_data.sh
+++ scripts/command_prep_data.sh
@@ -1,3 +1,2 @@
 #/bin/bash
-sudo /home/hqh2101/anaconda3/envs/robot-learning/bin/python \ 
-kitti/prepare_data.py --gen_val --gen_train --gen_val_rgb_detection --lidar_dir $(2)
+python kitti/prepare_data.py --gen_val --gen_train --gen_val_rgb_detection --lidar_dir velodyne/
